43e1a4e5afe8e6066c9be6cb2a1b00b7be97c462
Saba Chitorelidze <chitorelidze.99@gmail.com>
2025-08-31 19:57:03 +0400

    finished

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 export function Main({ children }) {
   return (
-    <main className="flex flex-col gap-2 w-full lg:flex-row lg:gap-8">
+    <main className="flex flex-col w-full gap-12 lg:flex-row lg:gap-8">
       {children}
     </main>
   );
